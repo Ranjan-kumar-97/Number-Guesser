d9bd14f9545e0fc5cf4209347c8eee0e16733a8c
Ranjan kumar <kranjan948@gmail.com>
2023-04-18 14:06:53 +0530

    second commit

--- number_guesser.py
+++ number_guesser.py
@@ -1,39 +1,40 @@
 import random
+
+def check_digit(num):
+    if num.isdigit():
+        return int(num)
+    else:
+        return False
+
 top_of_range = input("Type a number to be Maximum:  ")
 guess_count = 0
+user_guess = -1
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
-
+top_of_range = check_digit (top_of_range)
+if top_of_range is not False:
     if top_of_range <= 0:
         print("Please type number larger than 0 next time.\n")
         quit()
-
 else:
-    print("Please type a number next time.")
+    print("Please type a number next time.\n")
+    quit()
 
 
 random_number = random.randint(0,top_of_range)
 
-while True:
+
+while user_guess != random_number:
     guess_count +=1
     user_guess = input("Make a Guess:  ")
+    user_guess=check_digit(user_guess)
 
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
-
-    else:
+    if user_guess is False:
         print("Please type a number next time.\n")
         continue
-
-    if user_guess == random_number:
-        print("You got it!")
-        break
-
     elif user_guess > random_number:
         print("You were above the number!\n")
 
-    else:
+    elif user_guess < random_number:
         print("You were below the number!\n")
 
 print("You got it in", guess_count, "guesses.\n")
